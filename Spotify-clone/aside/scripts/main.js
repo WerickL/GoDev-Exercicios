015a42faa7fdf6717e9c46cb878adafe29b3488e
Werick.L <wericklima2019@gmail.com>
2022-06-30 09:19:02 -0300

    delete some folders

--- Spotify-clone/aside/scripts/main.js
+++
@@ -1,7 +0,0 @@
-//Abre e fecha o menu mobile e altera o formato do ícone de menu
-//Adicionadod aos links do menu, para ao serem clicados, fechar o menu
-const openCloseMenu = () => {
-  document.body.classList.toggle('menu-expanded')
-  hamburguer.classList.toggle('active')
-}
-hamburguer.addEventListener('click', openCloseMenu)
