3be7eb74ef70531536e75cfc9008661ea499e65b
Werick.L <wericklima2019@gmail.com>
2022-07-27 10:00:11 -0300

    versaofuncionando independente

---
+++ Hackadev/sacolaCompras/assets/scripts/sacolaCompras.js
@@ -0,0 +1,24 @@
+//elementos
+const botaoAbrirSacola = document.getElementById('nomeDoID')
+
+const body = document.body
+
+let botaoFecharSacola = document.getElementById('fecharSacola');
+
+const sacola = document.getElementById('sacola')
+
+
+//eventos
+botaoFecharSacola.addEventListener('click', fecharSacola)
+botaoAbrirSacola.addEventListener('click', abrirSacola)
+
+
+//funções
+function fecharSacola() {
+  sacola.classList.remove('visivel')
+  body.classList.remove('fixed')
+}
+function abrirSacola() {
+  sacola.classList.add('visivel')
+  body.classList.add('fixed')
+}
